f3d5f99b439a8bb27d64b1f33fab7355d670451e
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-20 17:17:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,8 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash.empty?
+  tmp_value = nil
+  tmp_key = nil
   name_hash.each do |key, value|
     if !tmp_value
       tmp_value = value
